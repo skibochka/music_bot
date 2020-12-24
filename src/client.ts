d8c1e7f8c9922837b349f0869d21f9873c503ae6
BOSS <boss@MacBook-Air-BOSS.local>
2020-12-24 21:43:11 +0200

    Added emoji

--- src/client.ts
+++ src/client.ts
@@ -18,7 +18,7 @@ export default class Client {
   }
 
   async start(msg) {
-    return this.bot.sendMessage(msg.chat.id, 'Отправь мне имя пользователя или ссылку на его профиль');
+    return this.bot.sendMessage(msg.chat.id, 'Отправь мне ссылку на YouTube видео и я верну тебе аудио дорожу с него 😎');
   }
 
   async convert(msg) {
@@ -27,17 +27,19 @@ export default class Client {
       const id = await ytdl.getURLVideoID(msg.text);
       const {videoDetails: {title, lengthSeconds}} = await ytdl.getInfo(id);
 
-      if (Number(lengthSeconds) > 600) return this.bot.sendMessage(msg.chat.id, 'Извините но видео слишком длинное');
+      if (Number(lengthSeconds) > 600) return this.bot.sendMessage(msg.chat.id, 'Извини но видео слишком длинное 🥺');
+      await this.bot.sendMessage(msg.chat.id, 'Погоди несколько секунд, обробатываю твой запрос 😉')
       const video = await ytdl(msg.text).pipe(fs.createWriteStream(`src/video/${name}.mp4`));
       video.on('finish', async () => {
         const process = await new ffmpeg(`src/video/${name}.mp4`);
         await process.fnExtractSoundToMP3(`src/sound/${name}.mp3`);
         fs.unlink(`src/video/${name}.mp4`,()=>{});
         await this.bot.sendAudio(msg.chat.id, `src/sound/${name}.mp3`, {}, { filename: title});
-        return fs.unlink(`src/sound/${name}.mp3`,()=>{});
+        fs.unlink(`src/sound/${name}.mp3`,()=>{});
+        return this.bot.sendMessage(msg.chat.id, 'Что дальше? 😃 ');
       });
     } catch (error) {
-      return this.bot.sendMessage(msg.chat.id, 'Видео не найдено');
+      return this.bot.sendMessage(msg.chat.id, 'Видео не найдено 🤔');
     }
   }
 }
